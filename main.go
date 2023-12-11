ea917ab3fd1b418e9ca191e0623c7824ab5a27b9
Arjuna Satria <arjunasatria1717@gmail.com>
2023-12-11 12:09:17 +0700

    edit: func to static

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strconv"
 
-	// "net/http"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
@@ -104,52 +105,180 @@ func initDB() {
 // CRUD operations for Customer, Product, and Transaction
 
 // Customer API Handlers
+
+// createCustomer creates a new customer.
 func createCustomer(c *gin.Context) {
-	// Implement your logic to create a new customer
+	var customer Customer
+	if err := c.ShouldBindJSON(&customer); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Implement your logic to save the customer to the database
+	// ...
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Customer created successfully", "data": customer})
 }
 
+// getCustomer retrieves a customer by ID.
 func getCustomer(c *gin.Context) {
-	// Implement your logic to get a customer by ID
+	id := c.Param("id")
+
+	// Convert the ID to an integer
+	customerID, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid customer ID"})
+		return
+	}
+
+	// Query to fetch a customer by ID
+	row := db.QueryRow("SELECT id, name, phone_number, address FROM customers WHERE id = $1", customerID)
+
+	// Create a Customer object to hold the result
+	var customer Customer
+
+	// Scan the row data into the customer object
+	err = row.Scan(&customer.ID, &customer.Name, &customer.PhoneNumber, &customer.Address)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Customer not found"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Customer retrieved successfully", "data": customer})
 }
 
+// updateCustomer updates a customer by ID.
 func updateCustomer(c *gin.Context) {
-	// Implement your logic to update a customer by ID
+	id := c.Param("id")
+	var updatedCustomer Customer
+	if err := c.ShouldBindJSON(&updatedCustomer); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Implement your logic to update the customer in the database by ID
+	// ...
+
+	c.JSON(http.StatusOK, gin.H{"message": "Customer updated successfully", "data": updatedCustomer})
 }
 
+// deleteCustomer deletes a customer by ID.
 func deleteCustomer(c *gin.Context) {
-	// Implement your logic to delete a customer by ID
+	id := c.Param("id")
+
+	// Implement your logic to delete the customer from the database by ID
+	// ...
+
+	c.JSON(http.StatusOK, gin.H{"message": "Customer deleted successfully", "data": "OK"})
 }
 
 // Product API Handlers
+
+// createProduct creates a new product.
 func createProduct(c *gin.Context) {
-	// Implement your logic to create a new product
+	var product Product
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Implement your logic to save the product to the database
+	// ...
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Product created successfully", "data": product})
 }
 
+// listProducts lists all products.
 func listProducts(c *gin.Context) {
-	// Implement your logic to list all products
+	// Implement your logic to fetch all products from the database
+	// ...
+
+	// Example response (replace with actual fetched data)
+	products := []Product{
+		{ID: 1, Name: "Product A", Price: 50, Unit: "Unit"},
+		{ID: 2, Name: "Product B", Price: 30, Unit: "Kg"},
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Products retrieved successfully", "data": products})
 }
 
+// getProduct retrieves a product by ID.
 func getProduct(c *gin.Context) {
-	// Implement your logic to get a product by ID
+	id := c.Param("id")
+
+	// Implement your logic to fetch the product from the database by ID
+	// ...
+
+	// Example response (replace with actual fetched data)
+	product := Product{ID: 1, Name: "Product A", Price: 50, Unit: "Unit"}
+	c.JSON(http.StatusOK, gin.H{"message": "Product retrieved successfully", "data": product})
 }
 
+// updateProduct updates a product by ID.
 func updateProduct(c *gin.Context) {
-	// Implement your logic to update a product by ID
+	id := c.Param("id")
+	var updatedProduct Product
+	if err := c.ShouldBindJSON(&updatedProduct); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Implement your logic to update the product in the database by ID
+	// ...
+
+	c.JSON(http.StatusOK, gin.H{"message": "Product updated successfully", "data": updatedProduct})
 }
 
+// deleteProduct deletes a product by ID.
 func deleteProduct(c *gin.Context) {
-	// Implement your logic to delete a product by ID
+	id := c.Param("id")
+
+	// Implement your logic to delete the product from the database by ID
+	// ...
+
+	c.JSON(http.StatusOK, gin.H{"message": "Product deleted successfully", "data": "OK"})
 }
 
 // Transaction API Handlers
+
+// createTransaction creates a new transaction.
 func createTransaction(c *gin.Context) {
-	// Implement your logic to create a new transaction
+	var transaction Transaction
+	if err := c.ShouldBindJSON(&transaction); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Implement your logic to save the transaction to the database
+	// ...
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Transaction created successfully", "data": transaction})
 }
 
+// getTransaction retrieves a transaction by ID.
 func getTransaction(c *gin.Context) {
-	// Implement your logic to get a transaction by ID
+	id := c.Param("id")
+
+	// Implement your logic to fetch the transaction from the database by ID
+	// ...
+
+	// Example response (replace with actual fetched data)
+	transaction := Transaction{ID: 1, BillDate: "2023-01-01", EntryDate: "2023-01-01", FinishDate: "2023-01-02", EmployeeID: "emp123", CustomerID: "cust456"}
+	c.JSON(http.StatusOK, gin.H{"message": "Transaction retrieved successfully", "data": transaction})
 }
 
+// listTransactions lists transactions based on query parameters.
 func listTransactions(c *gin.Context) {
-	// Implement your logic to list transactions based on query parameters
+	// Implement your logic to fetch transactions from the database based on query parameters
+	// ...
+
+	// Example response (replace with actual fetched data)
+	transactions := []Transaction{
+		{ID: 1, BillDate: "2023-01-01", EntryDate: "2023-01-01", FinishDate: "2023-01-02", EmployeeID: "emp123", CustomerID: "cust456"},
+		{ID: 2, BillDate: "2023-01-02", EntryDate: "2023-01-02", FinishDate: "2023-01-03", EmployeeID: "emp456", CustomerID: "cust789"},
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Transactions retrieved successfully", "data": transactions})
 }
