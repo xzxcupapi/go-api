c7d66143e3282e90f380e3c3d678c0706001820a
Arjuna Satria <arjunasatria1717@gmail.com>
2023-12-13 09:38:43 +0700

    finalize API

--- handlers/transaction.go
+++ handlers/transaction.go
@@ -6,47 +6,45 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 // Transaction Struct
 type Transaction struct {
 	Id         int       `json:"id"`
-	BillDate   time.Time `json:"billdate"`
-	EntryDate  time.Time `json:"entrydate"`
-	FinishDate time.Time `json:"finishdate"`
+	BillDate   time.Time `json:"billdate" format:"2006-01-02"`
+	EntryDate  time.Time `json:"entrydate" format:"2006-01-02"`
+	FinishDate time.Time `json:"finishdate" format:"2006-01-02"`
 	EmployeeId int       `json:"employeeid"`
 	CustomerId int       `json:"customerid"`
 }
 
 // CreateTransaction
+// CreateTransaction
 func CreateTransaction(c *gin.Context, db *sql.DB) {
 	var transaction Transaction
-	if err := c.ShouldBindJSON(&transaction); err != nil {
+	if err := c.ShouldBindWith(&transaction, binding.JSON); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
-	// Format dates
-	formattedBillDate := transaction.BillDate.Format("2006-01-02")
-	formattedEntryDate := transaction.EntryDate.Format("2006-01-02")
-	formattedFinishDate := transaction.FinishDate.Format("2006-01-02")
-
-	query := "INSERT INTO transaction (id, billdate, entrydate, finishdate, emploeeid, customerid) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id"
+	query := "INSERT INTO transactions (id, billdate, entrydate, finishdate, employeeid, customerid) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id"
 	stmt, err := db.Prepare(query)
 	if err != nil {
-		c.JSON(500, gin.H{"error": "Query Not Valid"})
+		c.JSON(500, gin.H{"error": "Failed to prepare the database query"})
+		return
 	}
 	defer stmt.Close()
 
-	//Log
+	// Log
 	fmt.Println("SQL Query", query)
 
-	//Execute SQL Query
-	err = stmt.QueryRow(&transaction.Id, formattedBillDate, formattedEntryDate, formattedFinishDate, &transaction.EmployeeId, &transaction.CustomerId).Scan(&transaction.Id)
+	// Execute SQL Query
+	err = stmt.QueryRow(&transaction.Id, &transaction.BillDate, &transaction.EntryDate, &transaction.FinishDate, &transaction.EmployeeId, &transaction.CustomerId).Scan(&transaction.Id)
 	if err != nil {
-		c.JSON(500, gin.H{"error": "Failed Insert Transaction"})
+		c.JSON(500, gin.H{"error": "Failed to insert transaction into the database"})
 		return
 	}
 
-	c.JSON(201, gin.H{"data": transaction, "message": "Insert Products Successfully"})
+	c.JSON(201, gin.H{"data": transaction, "message": "Insert Transaction Successfully"})
 }
